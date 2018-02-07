bef114aadf7bd456d06dc5c249887265b4fbdeca
DESKTOP-IERPP2I\Meciry <m.cengiz4716@gmail.com>
2018-02-07 22:40:01 +0300

    Quest cpp added to file.

---
+++ Source/InventorySystem/InventorySystemPlugin/Quest.h
@@ -0,0 +1,32 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+#include "Engine/Texture2D.h"
+#include "Engine/DataTable.h"
+#include "Quest.generated.h"
+
+UENUM(BlueprintType)
+enum class EQuestStatus : uint8 {
+	ACTIVE		UMETA(DisplayName = "Active"),
+	FINISHED	UMETA(DisplayName = "Finished"),
+	FAILED		UMETA(DisplayName = "Failed")
+};
+
+USTRUCT(BlueprintType)
+struct FQuest {
+
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Details")
+	FText QuestName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Details")
+	TEnumAsByte<EQuestStatus>ItemType;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Item Details")
+	FText QuestDetail;
+
+};
+
