22c27562c861bbc2dad8912423649baa936f1c6f
Mehmet Cengiz <m.cengiz4716@gmail.com>
2018-03-11 19:33:01 +0300

    Turning BP to Cpp -> InventoryWidget class skeleton created and improved.

--- Source/InventorySystem/InventorySystemPlugin/UserWidgets/InventoryWidget.cpp
+++ Source/InventorySystem/InventorySystemPlugin/UserWidgets/InventoryWidget.cpp
@@ -2,6 +2,45 @@
 
 #include "InventoryWidget.h"
 
+#include "UObject/ConstructorHelpers.h"
+
+#include "Components/Button.h"
+#include "Components/WrapBox.h"
+
+#include "ItemWidget.h"
+
+UInventoryWidget::UInventoryWidget(const FObjectInitializer & ObjectInitializer) : Super(ObjectInitializer) {
+	ConstructorHelpers::FClassFinder<UUserWidget> InventoryItemBPClass(TEXT("/Game/InventorySystem/MIKUI/SubWidgets/Inventory/WBP_Item"));
+	if (!ensure(InventoryItemBPClass.Class != nullptr)) return;
+	InventoryItemClass = InventoryItemBPClass.Class;
+}
+
+bool UInventoryWidget::Initialize() {
 
 
+	return false;
+}
 
+void UInventoryWidget::OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld) {
+	
+}
+
+void UInventoryWidget::GetCharacterInventoryRef() {
+	
+}
+
+void UInventoryWidget::CreateItemSlots() {
+	
+}
+
+void UInventoryWidget::RefreshInventory() {
+	
+}
+
+void UInventoryWidget::SwapItemsBySlot() {
+	
+}
+
+void UInventoryWidget::ChangeItemSlot() {
+	
+}
--- Source/InventorySystem/InventorySystemPlugin/UserWidgets/InventoryWidget.h
+++ Source/InventorySystem/InventorySystemPlugin/UserWidgets/InventoryWidget.h
@@ -14,7 +14,22 @@ class INVENTORYSYSTEM_API UInventoryWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
-	
+public:
+	UInventoryWidget(const FObjectInitializer& ObjectInitializer);
+
+protected:
+	virtual bool Initialize() override;
+	void OnLevelRemovedFromWorld(ULevel* InLevel, UWorld* InWorld) override;
+
+private:
+
+	TSubclassOf<class UUserWidget> InventoryItemClass;
+
+	void GetCharacterInventoryRef();
+	void CreateItemSlots();
+	void RefreshInventory();
+	void SwapItemsBySlot();
+	void ChangeItemSlot();
 	
 	
 };
