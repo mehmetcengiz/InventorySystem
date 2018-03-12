e86dac6a6998e4034aa64739864a19087c96afb0
Mehmet Cengiz <m.cengiz4716@gmail.com>
2018-03-12 23:07:59 +0300

    Item widget proporties added.

--- Source/InventorySystem/InventorySystemPlugin/UserWidgets/ItemWidget.h
+++ Source/InventorySystem/InventorySystemPlugin/UserWidgets/ItemWidget.h
@@ -4,7 +4,13 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+
+#include "InventorySystemPlugin/Item.h"
+
+
 #include "ItemWidget.generated.h"
+
+class UInventoryWidget;
 
 /**
  * 
@@ -14,7 +20,24 @@ class INVENTORYSYSTEM_API UItemWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+
+protected:
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite)
+	UTexture2D* ImgItem;
+
+	UPROPERTY(meta = (BindWidget), BlueprintReadWrite)
+	FText TxtQuantity;
 	
+	UPROPERTY(BlueprintReadWrite)
+	FItem ItemInfo;
 	
+	UPROPERTY(BlueprintReadWrite)
+	int32 SlotIndex;
+
+	UPROPERTY(BlueprintReadWrite)
+	bool bIsSlotHasItem;
+
+	UPROPERTY(BlueprintReadWrite)
+	UInventoryWidget* InventoryWidgetRef;
 	
 };
